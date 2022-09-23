522eb22d0e6180fe0bdef0ba30c7d8538f9868bf
rsrchtsm <113302177+rsrchtsm@users.noreply.github.com>
2022-09-24 03:54:14 +0530

    Add files via upload

---
+++ models/SingTplusPhi_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.3.0 for Linux x86 (64-bit) (May 10, 2021)
+# Date: Fri 23 Sep 2022 16:57:49
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
